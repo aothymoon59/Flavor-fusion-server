6864b90446c0f8d309c6f2e2e0743b1c811651e8
aothymoon59 <mmoon8745@gmail.com>
2023-05-01 23:11:18 +0600

    chefs data added and api created

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require("./data/chefs.json");
+
 app.get("/", (req, res) => {
   res.send("Flavor Fusion is running");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
