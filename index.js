fb723b617f0efecea3496dd2b3fd780147b4e604
aothymoon59 <mmoon8745@gmail.com>
2023-05-02 01:41:24 +0600

    all recipes api created

--- index.js
+++ index.js
@@ -3,19 +3,36 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
 
 app.get("/", (req, res) => {
   res.send("Flavor Fusion is running");
 });
 
+// all chefs api
 app.get("/chefs", (req, res) => {
   res.send(chefs);
 });
 
+// single chef api by id
 app.get("/chefs/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const singleChef = chefs.find((chef) => parseInt(chef.id) === id);
   res.send(singleChef);
+});
+
+// all recipes api
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+// chef recipes api
+app.get("/chef-recipe/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const chefRecipes = recipes.filter(
+    (recipe) => parseInt(recipe.chef_id) === id
+  );
+  res.send(chefRecipes);
 });
 
 app.listen(port, () => {
